7dd451257534a66b4bb4b2ceeac8da0f023e6860
扬 何 <1015485344@qq.com>
2017-09-09 22:12:16 +0800

    ppc

--- C++primer/C++primer.cpp
+++ C++primer/C++primer.cpp
@@ -32,22 +32,7 @@ void  debug( const Quote & item)
 
 int  main()
 {
-	vector<shared_ptr<Quote>> vec;
-	Quote qu1("happy",100);
-	Bulk_quote qu2("happd", 100, 10, 0.99);
-	half_quote qu3("bsgoas", 100, 10, 0.1);
-	Bulk_quote qu4("nimabsif", 100, 10, 0.7);
-	half_quote qus("alsf", 100, 10, .2);
-	vec.push_back(make_shared<Bulk_quote>(qu2));
-	vec.push_back(make_shared<half_quote>(qu3));
-	vec.push_back(make_shared<Quote>(qu4));
-	vec.push_back(make_shared<half_quote>(qus));
-	for (auto &m : vec){
-		double sum = 0;
-		sum += m->net_price(12);
-		cout << sum << endl;
-	}
-		
+	vector<String> herb;
 	system("pause");
     return 0;
 }
